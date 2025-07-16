6c288a6d00a543837df18b3a0910004e7bfda6c5
Pravin Singh Rana <dev.psrana@gmail.com>
2025-07-16 19:39:37 +0530

    added lead mail

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\LeadMail;
 
 class BackController extends Controller
 {
     public function booking(Request $request)
     {
-        return $request;
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'phone' => 'required|string|max:20',
+        ]);
+        Mail::to('info@broliontourism.com')->send(new LeadMail($validated));
 
     }
 }
---
+++ resources/views/emails/lead.blade.php
@@ -0,0 +1,4 @@
+<h2>New Lead Submitted</h2>
+<p><strong>Name:</strong> {{ $data['name'] }}</p>
+<p><strong>Email:</strong> {{ $data['email'] }}</p>
+<p><strong>Phone:</strong> {{ $data['phone'] }}</p>
