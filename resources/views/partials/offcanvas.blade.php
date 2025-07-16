9a0af54e8458e6af538489f81360e4c5ae780295
Pravin Singh Rana <dev.psrana@gmail.com>
2025-07-16 21:36:36 +0530

    fixed logo flash

--- resources/views/partials/offcanvas.blade.php
+++ resources/views/partials/offcanvas.blade.php
@@ -6,7 +6,7 @@
                  <div class="offcanvas__top d-flex justify-content-between align-items-center">
                      <div class="offcanvas__logo">
                          <a href="index.html">
-                             <img src="{{ asset('assets/img/logo.svg') }}" alt="edutec">
+                             <img src="{{ asset('assets/img/logo.png') }}" alt="edutec">
                          </a>
                      </div>
                      <div class="offcanvas__close">
